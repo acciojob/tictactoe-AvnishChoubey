d9401bb4534a4b3231a0970094b5ed2ebe8d731a
Avnish Choubey <118668098+AvnishChoubey@users.noreply.github.com>
2025-10-08 16:38:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const mainContainer = document.getElementByClass('container');
+const startBtn = document.getElementById('submit');
+startBtn.addEventListener('click', function(event) {
+	event.preventDefault();
+
+	const player1 = document.getElementById('player-1').value;
+	const player2 = document.getElementById('player-2').value;
+
+	mainContainer.innerHTML = "<p>'${player1},you're up</p>
+		";
+});
